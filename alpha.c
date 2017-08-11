ffd8e21c5b113121c2291ba8623684c811813081
durgakarthika <30343816+durgakarthika@users.noreply.github.com>
2017-08-11 16:42:22 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char b;
+printf("enter a character:");
+scanf("%c",&b);
+if( (b>=a&&b<=z) || (b>=A&&b<=Z) )
+printf("%c the word is an alphabet.",b);
+else
+printf("%c the word is not an alphabet.",b);
+return 0;
+}
+
